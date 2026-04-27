5a3a0261056a6867198d82f23c99151d5ca6d0d3
bsc-com-42-24 <bsc-com-42-24@unima.ac.mw>
2026-04-27 18:36:05 +0200

    Add Matching module changes and Auth DTOs

--- src/matching/matching.service.ts
+++ src/matching/matching.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MatchingService {}
+export class MatchingService {
+    recommendGroups(course: string) { 
+        return { message: 'Recommended groups for course', course }; 
+} 
+
+findUsers(skills: string) { 
+    return { message: 'Users with similar skills', skills }; 
+} 
+similarGroups(id: string) { 
+return { message: 'Similar groups found', id }; 
+} 
+joinSuggestions() { 
+return { message: 'Suggested groups to join' }; 
+} 
+}
